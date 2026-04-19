4a93358050b7238e8cd1e458d0a5e1ca44e4ae36
Lorik Hamiti <lorik.hamiti@student.uni-pr.edu>
2026-04-19 09:58:04 +0200

    Implement FileService with directory walking and listing

--- src/services/fileservice.js
+++ src/services/fileservice.js
@@ -1,1 +1,58 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
+import { resolveSafePath, toPortableRelativePath } from '../../shared/safe-paths.js';
+
+async function walkDirectory(directoryPath) {
+  const dirents = await fs.readdir(directoryPath, { withFileTypes: true });
+  const entries = [];
+
+  for (const dirent of dirents) {
+    const absolutePath = path.join(directoryPath, dirent.name);
+    entries.push({
+      dirent,
+      absolutePath,
+    });
+
+    if (dirent.isDirectory()) {
+      entries.push(...await walkDirectory(absolutePath));
+    }
+  }
+
+  return entries;
+}
+
+export class FileService {
+  constructor({ baseDir }) {
+    this.baseDir = baseDir;
+  }
+
+  async init() {
+    await fs.mkdir(this.baseDir, { recursive: true });
+  }
+
+  async list(relativePath = '.') {
+    const directoryPath = resolveSafePath(this.baseDir, relativePath);
+    const dirents = await fs.readdir(directoryPath, { withFileTypes: true });
+
+    const entries = await Promise.all(
+      dirents
+        .sort((left, right) => left.name.localeCompare(right.name))
+        .map(async (dirent) => {
+          const absolutePath = path.join(directoryPath, dirent.name);
+          const stats = await fs.stat(absolutePath);
+
+          return {
+            name: dirent.name,
+            type: dirent.isDirectory() ? 'directory' : 'file',
+            size: stats.size,
+            path: toPortableRelativePath(this.baseDir, absolutePath),
+          };
+        }),
+    );
+
+    return {
+      path: toPortableRelativePath(this.baseDir, directoryPath),
+      entries,
+    };
+  }
